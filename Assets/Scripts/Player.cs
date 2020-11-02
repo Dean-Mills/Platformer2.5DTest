30d041f090dc493721e1a3d5192d779ff730b229
Dean Mills <deansmills@gmail.com>
2020-11-02 22:29:24 +0200

    character controller setup with simple jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private float _gravity;
+    [SerializeField]
+    private float _jumpHeight;
+
+    private float yVal;
     void Start()
     {
-        
+        _controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var horizontal = Input.GetAxis("Horizontal");
+        var direction = new Vector3(horizontal, 0, 0);
+
+        if (!_controller.isGrounded)
+        {
+            yVal -= _gravity;
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+                yVal = _jumpHeight;
+        }
+        direction.y = yVal;
+        _controller.Move(direction * _speed * Time.deltaTime);
     }
 }
