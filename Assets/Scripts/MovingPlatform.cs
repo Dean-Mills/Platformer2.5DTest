244d979b9ca178039b1cbcb86cae6919070d59e5
Dean Mills <deansmills@gmail.com>
2020-11-03 22:39:04 +0200

    Moving platform

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _targetA;
+    [SerializeField]
+    private Transform _targetB;
+    [SerializeField]
+    private float _speed = 1f;
+
+    bool moveToB = true;
+    void Update()
+    {
+        
+        if(moveToB)
+            transform.position = Vector3.MoveTowards(transform.position, _targetB.position, _speed * Time.deltaTime);
+        else
+            transform.position = Vector3.MoveTowards(transform.position, _targetA.position, _speed * Time.deltaTime);
+
+        if (Mathf.Abs(transform.position.x - _targetB.position.x) < 0.001)
+            moveToB = false;
+        if (Mathf.Abs(transform.position.x - _targetA.position.x) < 0.001)
+            moveToB = true;
+    }
+}
